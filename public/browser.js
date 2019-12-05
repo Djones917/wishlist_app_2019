0e785fc141270801da21def04cea3df6b6960620
Derek <nightwing917@hotmail.com>
2019-12-04 18:55:32 -0700

    added promplt

--- public/browser.js
+++ public/browser.js
@@ -1,6 +1,6 @@
 document.addEventListener("click", function(e) {
     if(e.target.classList.contains("edit-me")) {
-        alert("You clicked the edit button!");
+        prompt("Enter you new text!");
     }
 });
 
