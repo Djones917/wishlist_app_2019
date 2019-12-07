45893f4bd0b64843d063cf50f0c96c0e956fe475
Derek <nightwing917@hotmail.com>
2019-12-06 19:17:03 -0700

    added innerhtml for list

--- public/browser.js
+++ public/browser.js
@@ -2,7 +2,8 @@ document.addEventListener("click", function(e) {
     if(e.target.classList.contains("edit-me")) {
         let userInput = prompt("Enter you new text!");
        axios.post('/update-item', {text: userInput, id: e.target.getAttribute("data-id")}).then(function() {
-           // Do something here interesting
+           // Do something interesting
+           e.target.parentElement.parentElement.querySelector(".item-text").innerHTML = userInput;
        }).catch(function() {
            console.log("Please try again later!");
        });
